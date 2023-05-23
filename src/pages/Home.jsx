0f5917f5cd1fc378542da5fc6783d8e51f8419f1
ICT ACADEMY <yazid>
2023-05-23 16:53:09 +0530

    h

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      {count > 0 && <button onClick={decrement}>Decrement</button>}
+    </div>
+  );
+};
+
+
+
+export default Counter;
+
+
+
+
+
